ea84a98ea9a1ef48d2b020398c055ff82c0fba09
Stanimir <stmbksa@gmail.com>
2023-03-20 17:44:23 +0200

    homework

---
+++ Objects and Classes/src/Lab/Student.java
@@ -0,0 +1,95 @@
+package Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String city;
+
+    public Student(String firstName, String lastName, int age, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Student setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Student setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Student setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Student setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Student> students = new ArrayList<>();
+
+        readAndFillStudentsList(scanner, students);
+
+        String filterCity = scanner.nextLine();
+
+        filterAndPrintStudentsByCity(students, filterCity);
+
+
+    }
+
+    private static void filterAndPrintStudentsByCity(List<Student> students, String filterCity) {
+        for (Student student : students) {
+            if (student.getCity().equals(filterCity)) {
+                System.out.printf("%s %s is %d years old%n", student.getFirstName(),
+                        student.getLastName(), student.getAge(), student.getCity());
+            }
+        }
+    }
+
+    private static void readAndFillStudentsList(Scanner scanner, List<Student> students) {
+        String line;
+        while (!"end".equals(line = scanner.nextLine())) {
+            String[] tokens = line.split("\\s+");
+
+            String firstName = tokens[0];
+            String lastName = tokens[1];
+            int age = Integer.parseInt(tokens[2]);
+            String city = tokens[3];
+
+            Student student = new Student(firstName, lastName, age, city);
+
+            students.add(student);
+        }
+    }
+}
