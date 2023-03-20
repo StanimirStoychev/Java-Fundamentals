021f97bfae0723ab32caf6703c73f2e7ae7b2d59
Stanimir <stmbksa@gmail.com>
2023-03-20 17:58:14 +0200

    update Student to Student 2.0

--- Objects and Classes/src/Lab/Student.java
+++ Objects and Classes/src/Lab/Student.java
@@ -68,6 +68,15 @@ public class Student {
 
     }
 
+    private static boolean isStudentExisting(List<Student> students, String firstName, String lastName) {
+        for (Student student : students) {
+            if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private static void filterAndPrintStudentsByCity(List<Student> students, String filterCity) {
         for (Student student : students) {
             if (student.getCity().equals(filterCity)) {
@@ -87,9 +96,29 @@ public class Student {
             int age = Integer.parseInt(tokens[2]);
             String city = tokens[3];
 
-            Student student = new Student(firstName, lastName, age, city);
+            if (isStudentExisting(students, firstName, lastName)) {
+                Student student = getStudent(students, firstName, lastName);
 
-            students.add(student);
+                student.setFirstName(firstName);
+                student.setLastName(lastName);
+                student.setAge(age);
+                student.setCity(city);
+            } else {
+                Student student = new Student(firstName, lastName, age, city);
+                students.add(student);
+            }
+
         }
+    }
+
+    private static Student getStudent(List<Student> students, String firstName, String lastName) {
+        Student existingStudent = null;
+
+        for (Student student : students) {
+            if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)) {
+                existingStudent = student;
+            }
+        }
+        return existingStudent;
     }
 }
