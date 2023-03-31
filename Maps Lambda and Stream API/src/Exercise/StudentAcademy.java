236b72ad41a2d54690950adf3d49cd60f0eb9d36
Stanimir <stmbksa@gmail.com>
2023-03-31 19:55:00 +0300

    homework

---
+++ Maps Lambda and Stream API/src/Exercise/StudentAcademy.java
@@ -0,0 +1,11 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class StudentAcademy {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
