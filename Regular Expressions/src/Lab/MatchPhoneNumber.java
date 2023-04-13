b4c2cc018ed5778fc791e83fee5f85710521cbee
Stanimir <stmbksa@gmail.com>
2023-04-13 13:24:21 +0300

    homework

--- Regular Expressions/src/Lab/MatchPhoneNumber.java
+++ Regular Expressions/src/Lab/MatchPhoneNumber.java
@@ -10,7 +10,7 @@ public class MatchPhoneNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String regex = "\\+359([- ])2\\1[\\d]{3}\\1[\\d]{4}\\b";
+        String regex = "\\+359([- ])2\\1\\d{3}\\1\\d{4}\\b";
         String phones = scanner.nextLine();
 
         Pattern pattern = Pattern.compile(regex);
